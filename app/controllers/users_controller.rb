cd58f1eb421b9e4832cccd43ee4ef1435ad87516
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-08 17:57:24 +0800

    create new users done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
 
 
-    validated :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
