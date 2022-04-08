b2f1972ee1adde5afc015ba27963c7baccb47116
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-08 17:31:42 +0800

    users seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+# The data can be loaded with the rails db:seed command (or created alongside the database with db:setup).
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(email: 'jj@gmail.com', username: 'oldbettie', password: 'chicken', admin: 'true')
+User.create(email: 'spongebob@gmail.com', username: 'spongebob', password: 'chicken')
+User.create(email: 'patrick@gmail.com', username: 'patrick', password: 'chicken')
+
+puts ":total seeded users is #{ User.count }"
