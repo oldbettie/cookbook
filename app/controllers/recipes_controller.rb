3b987b320b1d74677ead79e6e712c87261618d01
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-09 12:55:07 +0800

    recipe associations added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,10 +8,19 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
   def create
+    recipe = Recipe.create recipe_params
+    @current_user.recipe << recipe
+    redirect_to recipe
   end
 
   def edit
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :instructions, :image, :video, :category, :region)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,23 @@
 
 User.destroy_all
 
-User.create(email: 'jj@gmail.com', username: 'oldbettie', password: 'chicken', admin: 'true')
-User.create(email: 'spongebob@gmail.com', username: 'spongebob', password: 'chicken')
-User.create(email: 'patrick@gmail.com', username: 'patrick', password: 'chicken')
+u1 = User.create(email: 'jj@gmail.com', username: 'oldbettie', password: 'chicken', admin: 'true')
+u2 = User.create(email: 'spongebob@gmail.com', username: 'spongebob', password: 'chicken')
+u3 = User.create(email: 'patrick@gmail.com', username: 'patrick', password: 'chicken')
 
 puts ":total seeded users is #{ User.count }"
 
 
 # recipe seed data
 Recipe.destroy_all
-Recipe.create(name: 'meat pie', ingredients: 'groundbeef, gravy, pastry', instructions: 'bake at 220c for 120min', image: 'https://157212-453144-1-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/04/classic-meat-pies-1-1024x683@2x.jpg')
-Recipe.create(name: 'sausage roll', ingredients: 'sausages, pastry', instructions: 'bake at 220c for 120min', image: 'https://img.taste.com.au/kv8NEC44/w720-h480-cfill-q80/taste/2016/11/easy-sausage-rolls-28532-1.jpeg')
-Recipe.create(name: 'salad', ingredients: 'rabit food, salad dressing', instructions: 'slap it in a bowl and eat like a rabbit', image: 'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad-1.jpg')
+r1 = Recipe.create(name: 'meat pie', ingredients: 'groundbeef, gravy, pastry', instructions: 'bake at 220c for 120min', image: 'https://157212-453144-1-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/04/classic-meat-pies-1-1024x683@2x.jpg')
+r2 = Recipe.create(name: 'sausage roll', ingredients: 'sausages, pastry', instructions: 'bake at 220c for 120min', image: 'https://img.taste.com.au/kv8NEC44/w720-h480-cfill-q80/taste/2016/11/easy-sausage-rolls-28532-1.jpeg')
+r3 = Recipe.create(name: 'salad', ingredients: 'rabit food, salad dressing', instructions: 'slap it in a bowl and eat like a rabbit', image: 'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad-1.jpg')
 
 puts ":total seeded recipes is #{ Recipe.count }"
+
+puts 'users and recipes'
+
+u1.recipes << r1
+u2.recipes << r2
+u3.recipes << r3
