e9c327b852c1ad45edcd93e9022d8698a21c9177
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-11 07:22:58 +0800

    added image to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_022318) do
+ActiveRecord::Schema.define(version: 2022_04_10_232206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_04_09_022318) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.text "image"
   end
 
 end
