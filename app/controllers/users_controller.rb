55abd408fd5aca0553a2983a877ce25940939113
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-11 09:12:10 +0800

    profile image basic layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find params[:id]
   end
   def update
+    user = User.find params[:id]
+    user.update user_params
+    redirect_to user
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :image, :about)
   end
 end
